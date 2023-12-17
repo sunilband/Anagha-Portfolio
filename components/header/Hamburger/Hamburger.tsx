153c6539e8e108279f75a1a198fc78459368c4aa
Sunil Band <sunilband0007@gmail.com>
2023-12-17 12:02:43 +0530

    hamburger transparent blend

--- components/header/Hamburger/Hamburger.tsx
+++ components/header/Hamburger/Hamburger.tsx
@@ -20,9 +20,15 @@ const Hamburger = ({ navVisible, setNavVisible }: Props) => {
             whileInView={{ scale: 1, x: 0, y: 0 }}
             transition={{ duration: 0.5, type: "spring", stiffness: 80 }}
             exit={{ y: -290, x: 170, scale: 0, transformOrigin: "top" }}
-            className="absolute h-screen top-0 right-0 rounded-l-full w-[70vw] sm:w-[30vw] z-20 bg-blend-difference bg-slate-50  select-none shadow-2xl
+            className="absolute h-screen top-0 right-0 rounded-l-full w-[70vw] sm:w-[30vw] z-20 bg-white  select-none shadow-2xl
             max-h-screen max-w-screen overflow-hidden
+
             "
+            style={{
+              mixBlendMode: "screen",
+              backdropFilter: "blur(30px)",
+              WebkitBackdropFilter: "blur(30px)",
+            }}
           >
             <motion.div
               initial={{ opacity: 0, scale: 0, x: 0, y: 100, rotate: 180 }}
@@ -52,19 +58,22 @@ const Hamburger = ({ navVisible, setNavVisible }: Props) => {
                   initial={{ opacity: 0, x: -100 }}
                   whileInView={{ opacity: 1, x: 0 }}
                   transition={{ duration: 0.2, type: "spring", stiffness: 100 }}
-                  whileHover={{ 
+                  whileHover={{
                     scale: 1.1,
-                    x:-10,
+                    x: -10,
                     transition: { duration: 0.1 },
-                    shadow: "0px 0px 10px 0px rgba(0,0,0,0.75)"
+                    shadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
                   }}
                 >
                   <Link
                     href="/"
-                    className="fiexenFont text-black hover:text-[#0028ff]/70 "
+                    className="fiexenFont text-black"
                     onClick={() => setNavVisible(false)}
                   >
-                   <span className="text-[0.3em] mr-2 montserratFont text-black">01</span>Home
+                    <span className="text-[0.3em] mr-2 montserratFont text-black">
+                      01
+                    </span>
+                    Home
                   </Link>
                 </motion.div>
 
@@ -72,18 +81,21 @@ const Hamburger = ({ navVisible, setNavVisible }: Props) => {
                   initial={{ opacity: 0, y: 100 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.3, type: "spring", stiffness: 100 }}
-                  whileHover={{ 
+                  whileHover={{
                     scale: 1.1,
-                    x:-10,
+                    x: -10,
                     transition: { duration: 0.1 },
                   }}
                 >
                   <Link
                     href="/projects"
-                    className="fiexenFont text-black hover:text-[#0028ff]/70"
+                    className="fiexenFont text-black"
                     onClick={() => setNavVisible(false)}
                   >
-                    <span className="text-[0.3em] mr-2 montserratFont text-black">02</span>Work
+                    <span className="text-[0.3em] mr-2 montserratFont text-black">
+                      02
+                    </span>
+                    Work
                   </Link>
                 </motion.div>
 
@@ -91,18 +103,21 @@ const Hamburger = ({ navVisible, setNavVisible }: Props) => {
                   initial={{ opacity: 0, x: 100 }}
                   whileInView={{ opacity: 1, x: 0 }}
                   transition={{ duration: 0.4, type: "spring", stiffness: 100 }}
-                  whileHover={{ 
+                  whileHover={{
                     scale: 1.1,
-                    x:-10,
+                    x: -10,
                     transition: { duration: 0.1 },
                   }}
                 >
                   <Link
                     href="/projects"
-                    className="fiexenFont text-black hover:text-[#0028ff]/70"
+                    className="fiexenFont text-black"
                     onClick={() => setNavVisible(false)}
                   >
-                    <span className="text-[0.3em] mr-2 montserratFont text-black">03</span>About
+                    <span className="text-[0.3em] mr-2 montserratFont text-black">
+                      03
+                    </span>
+                    About
                   </Link>
                 </motion.div>
 
@@ -110,18 +125,21 @@ const Hamburger = ({ navVisible, setNavVisible }: Props) => {
                   initial={{ opacity: 0, y: -100 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.5, type: "spring", stiffness: 100 }}
-                  whileHover={{ 
+                  whileHover={{
                     scale: 1.1,
-                    x:-10,
+                    x: -10,
                     transition: { duration: 0.1 },
                   }}
-                >   
+                >
                   <Link
                     href="/"
-                    className="fiexenFont text-black hover:text-[#0028ff]/70"
+                    className="fiexenFont text-black"
                     onClick={() => setNavVisible(false)}
                   >
-                    <span className="text-[0.3em] mr-2 montserratFont text-black">04</span>Contact
+                    <span className="text-[0.3em] mr-2 montserratFont text-black">
+                      04
+                    </span>
+                    Contact
                   </Link>
                 </motion.div>
               </div>
