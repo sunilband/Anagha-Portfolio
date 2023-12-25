3ad0c96a7a550c422ad56992ab31df573f1f4817
Sunil Band <sunilband0007@gmail.com>
2023-12-25 13:12:20 +0530

    emails fixed

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -6,12 +6,21 @@ const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const { email, name, message } = await req.json();
-    const data = await resend.emails.send({
-      from: "Anagha Yawale <anagha@anagha.tech>",
-      to: email,
-      subject: "Contact Form Submission",
-      html: templete(name, email),
-    });
+    const data = await resend.batch.send([
+      {
+        from: "Anagha Yawale <anagha@anagha.tech>",
+        to: email,
+        subject: "Contact Form Submission",
+        html: templete(name, email),
+      },
+      {
+        from: `${name} <anagha@anagha.tech>`,
+        to: "anaghayawale7@gmail.com",
+        subject: "Contact Form Submission",
+        text: `You got a message from ${name} (${email})\n\n${message}`,
+      },
+    ]);
+
     return Response.json(data);
   } catch (error) {
     console.log(error);
